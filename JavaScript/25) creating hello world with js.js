1041009661d4a49e06d1ef06988646185f8050a4
Neha Mehar <nehamehar31@gmail.com>
2025-06-25 16:53:45 +0530

    added

---
+++ JavaScript/25) creating hello world with js.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Namaste React</title>
+</head>
+<body>
+    {/* <div id="root"></div>
+    <script> 
+        let x = document.createElement("h1")
+        x.innerHTML="hello world"
+
+        let y = document.getElementById("root")
+        y.appendChild(x)
+    </script> */}
+</body>
+</html>
